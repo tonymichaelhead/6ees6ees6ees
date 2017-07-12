313d4e42e9a0bd159fdded144732c15c975b006f
tonymichaelhead <tonymichaelhead@gmail.com>
2017-07-11 22:31:18 -0700

    Add ForagerBee subclass

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,8 +1,12 @@
 class ForagerBee extends Bee{
   constructor() {
+    super();
     this.age = 10;
     this.job = 'find pollen';
     this.canFly = true;
     this.treasureChest = [];
+  }
+  forage(treasure) {
+    this.treasureChest.push(treasure);
   }
 };
